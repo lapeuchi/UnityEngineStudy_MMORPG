33ecf215d05d50c6454405159cba93600f9dcc79
lapeuchi <78643629+lapeuchi@users.noreply.github.com>
2022-01-13 17:21:16 +0900

    sad
    sad

--- MMORPG_Study/Assets/Scripts/PlayerController.cs
+++ MMORPG_Study/Assets/Scripts/PlayerController.cs
@@ -3,28 +3,60 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
-{
+{   
+    [SerializeField]
     float _speed = 10.0f;
+
     void Start()
     {
-        
+   
     }
 
-    // GameObject (Player)
-        // Transform
-        // PlayerController (*)
+    float _yAngle = 0.0f;
     void Update()
     {
-        // deltaTime: 지난 시간 프레임 이후 경과된 시간(설명 직역), 간단히 설명해 시간.
+        // _yAngle += Time.deltaTime * 100.0f;
+        // 절대 회전값
+        // transform.eulerAngles = new Vector3(0.0f, _yAngle, 0.0f);
+        // +- delta
+        // transform.Rotate(new Vector3(0.0f, Time.deltaTime * 100.0f, 0.0f));
 
-        //방향 * 시간 * 속력
+        // transform.rotation = Quaternion.Euler(new Vector3(0.0f, _yAngle, 0.0f));
+        // vector3로 각도를 변경하면 짐벌락(두 축이 겹쳐서 회전이 안됨)이 발생해 문제가됨
+        // 그래서 게임엔진은 쿼터니언을 사용한다.
+
+        // transform.rotation
         if (Input.GetKey(KeyCode.W))
-            transform.position += new Vector3(0.0f, 0.0f, 1.0f) * Time.deltaTime * _speed;
+        {
+            // transform.rotation = Quaternion.LookRotation(Vector3.forward);
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.forward), 0.2f);
+            transform.position += Vector3.forward * Time.deltaTime * _speed;
+            //transform.Translate(Vector3.forward * Time.deltaTime * _speed);
+        }
+
         if (Input.GetKey(KeyCode.S))
-            transform.position += new Vector3(0.0f, 0.0f, -1.0f) * Time.deltaTime * _speed;
+        {
+            //transform.rotation = Quaternion.LookRotation(Vector3.back);
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.back), 0.2f);
+            transform.position += Vector3.back * Time.deltaTime * _speed;
+            //transform.Translate(Vector3.forward * Time.deltaTime * _speed);
+        }
+
         if (Input.GetKey(KeyCode.A))
-            transform.position += new Vector3(-1.0f, 0.0f, 0.0f) * Time.deltaTime * _speed;
+        {
+            //transform.rotation = Quaternion.LookRotation(Vector3.left);
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.left), 0.2f);
+            transform.position += Vector3.left * Time.deltaTime * _speed;
+            //transform.Translate(Vector3.forward * Time.deltaTime * _speed);
+        }
+            
         if (Input.GetKey(KeyCode.D))
-            transform.position += new Vector3(1.0f, 0.0f, 0.0f) * Time.deltaTime * _speed;
+        {
+            //transform.rotation = Quaternion.LookRotation(Vector3.right);
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.right), 0.2f);
+            transform.position += Vector3.right * Time.deltaTime * _speed;
+            //transform.Translate(Vector3.forward * Time.deltaTime * _speed);
+        }
+        
     }
 }
