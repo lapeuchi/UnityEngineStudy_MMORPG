5faad4135f24cfb0833ac2180c9344b7d33969ba
lapeuchi <78643629+lapeuchi@users.noreply.github.com>
2022-01-26 19:29:38 +0900

    df
    sdf

--- MMORPG_Study/Assets/Scripts/Controllers/PlayerController.cs
+++ MMORPG_Study/Assets/Scripts/Controllers/PlayerController.cs
@@ -11,9 +11,6 @@ public class PlayerController : MonoBehaviour
     {
         Managers.Input.MouseAction -= OnMouseClicked;
         Managers.Input.MouseAction += OnMouseClicked;
-
-        //TEMP
-        Managers.UI.ShowSceneUI<UI_Inven>();
     }
 
     public enum PlayerState
---
+++ MMORPG_Study/Assets/Scripts/Scenes/GameScene.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameScene : BaseScene
+{
+    private void Awake()
+    {
+        Init();
+    }
+
+    protected override void Init()
+    {
+        base.Init();
+
+        SceneType = Define.Scene.Game;
+
+        Managers.UI.ShowSceneUI<UI_Inven>();
+        
+    }
+
+    public override void Clear()
+    {
+        
+    }
+
+    
+}
